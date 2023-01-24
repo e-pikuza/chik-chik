ad9690552feab1f545e814735350df2a6da85d37
e-pikuza <pikuzo@mail.ru>
2023-01-24 18:30:15 +0300

    Add getting data from server. Add reserve form submit. Minor style changes.

---
+++ js/modules/const.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://highfalutin-fantasy-possum.glitch.me/';
--- js/modules/initSlider.js
+++ js/modules/initSlider.js
@@ -1,12 +1,4 @@
-'use strict';
-
-const addPreload = (elem) => {
-    elem.classList.add('preload')
-};
-
-const removePreload = (elem) => {
-    elem.classList.remove('preload')
-};
+import { addPreload, removePreload } from "./util.js";
 
 const startSlider = () => {
     const sliderItems = document.querySelectorAll('.slider__item');
@@ -69,7 +61,7 @@ const startSlider = () => {
     });
 };
 
-const initSlider = () => {
+export const initSlider = () => {
     const slider = document.querySelector('.slider');
     const sliderContainer = document.querySelector('.slider__container');
     
@@ -83,8 +75,3 @@ const initSlider = () => {
         startSlider();        
     });    
 };
-
-//window.addEventListener('DOMContentLoaded', initSlider);
-initSlider();
-
-
